73714bde51e6031d5f94a39281145307976b1893
ro-Me1 <ochieng.erwin@strathmore.edu>
2026-06-25 06:05:06 +0300

    Contact form logic

--- CleanHouse/JavaScript/script.js
+++ CleanHouse/JavaScript/script.js
@@ -1,1 +1,24 @@
-console.log("CleanHouse website loaded successfully");
+const form = document.getElementById("contactForm");
+
+if(form){
+
+form.addEventListener("submit", function(event){
+
+event.preventDefault();
+
+let name = document.getElementById("name").value;
+let email = document.getElementById("email").value;
+
+if(name === "" || email === ""){
+
+alert("Please fill all required fields.");
+
+return;
+
+}
+
+alert("Form submitted successfully!");
+
+});
+
+}
